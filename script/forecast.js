8ce133af190553e7b2203b83d5c4d601e43c397c
Szymon Dolnik <szymon.dolnik@gmail.com>
2021-06-28 21:01:51 +0200

    Repaired wrong API base URL

--- script/forecast.js
+++ script/forecast.js
@@ -4,7 +4,7 @@ const key = "GnZKLE2NlPN1qHw1Io5b3poxBaXAvqCm";
 // function to get information about city(cityKey) from API
 const getCity = async city => {
 
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
@@ -17,7 +17,7 @@ const getCity = async city => {
 //function to get current conditions in city we are searching for
 const getWeather = async cityKey => {
 
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${cityKey}?apikey=${key}`;
 
   const response = await fetch(base + query);
